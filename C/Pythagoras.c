ff3b3557b418d7b75262ef1afb0d4e144999ce76
Kurohaku0218 <96249252+Kurohaku0218@users.noreply.github.com>
2023-05-25 19:12:25 +0700

    Create Pythagoras.c

---
+++ C/Pythagoras.c
@@ -0,0 +1,37 @@
+/*
+Code written by Noxius (Adam)
+Date: 2023/05/25 (木)
+Complete at 18:24 WIB
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+
+    int a, b, c;
+
+    printf("Input sisi A: ");
+    scanf("%d", &a);
+
+    printf("Input sisi B: ");
+    scanf("%d", &b);
+
+    //Rumus Pythagoras
+    c = pow(a, 2) + pow(b, 2);
+
+    //Inisialisasi Hasil
+    printf("\t\tPerhitungan\t\t\n");
+    printf("A^2 = %d^2\n", a);
+    printf("B^2 = %d^2\n", b);
+    printf("C^2 = A^2 + B^2\n");
+    printf("C^2 = %d^2 + %d^2\n", a, b);
+    printf("C^2 = %1.f + %1.f = %d", pow(a, 2), pow(b, 2), c);
+    printf("\n\n");
+
+    //Output Hasil
+    printf("Akar dari %d adalah %1.f\n", c, sqrt(c));
+    printf("Hasil Pythagoras: %1.f", sqrt(c));
+
+    return 0;
+}
