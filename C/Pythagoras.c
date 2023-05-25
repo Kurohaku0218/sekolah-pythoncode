c9821acb3de5bf8675420427d969f2385f89ce3a
Kurohaku0218 <96249252+Kurohaku0218@users.noreply.github.com>
2023-05-25 20:35:09 +0700

    Update While Loop Pythagoras.c
    Penambahan while loop sehingga user dapat menghitung atau melanjutkan perhitungan lagi

--- C/Pythagoras.c
+++ C/Pythagoras.c
@@ -2,6 +2,7 @@
 Code written by Noxius (Adam)
 Date: 2023/05/25 (木)
 Complete at 18:24 WIB
+Rev1 at 20:21 WIB
 */
 
 #include<stdio.h>
@@ -9,29 +10,44 @@ Complete at 18:24 WIB
 
 int main() {
 
-    int a, b, c;
+    int a, b, c, ulang;
+    char lanjut = 'y';
 
-    printf("Input sisi A: ");
-    scanf("%d", &a);
+    while(lanjut == 'y'){
+        printf("Input sisi A: ");
+        scanf("%d", &a);
 
-    printf("Input sisi B: ");
-    scanf("%d", &b);
+        printf("Input sisi B: ");
+        scanf("%d", &b);
 
-    //Rumus Pythagoras
-    c = pow(a, 2) + pow(b, 2);
+        //Rumus Pythagoras
+        c = pow(a, 2) + pow(b, 2);
+        
+        //Inisialisasi Hasil
+        printf("\t\tPerhitungan\t\t\n");
+        printf("A^2 = %d^2\n", a);
+        printf("B^2 = %d^2\n", b);
+        printf("C^2 = A^2 + B^2\n");
+        printf("C^2 = %d^2 + %d^2\n", a, b);
+        printf("C^2 = %1.f + %1.f = %d", pow(a, 2), pow(b, 2), c);
+        printf("\n\n");
 
-    //Inisialisasi Hasil
-    printf("\t\tPerhitungan\t\t\n");
-    printf("A^2 = %d^2\n", a);
-    printf("B^2 = %d^2\n", b);
-    printf("C^2 = A^2 + B^2\n");
-    printf("C^2 = %d^2 + %d^2\n", a, b);
-    printf("C^2 = %1.f + %1.f = %d", pow(a, 2), pow(b, 2), c);
-    printf("\n\n");
+        //Output Hasil
+        printf("Akar dari %d adalah %1.f\n", c, sqrt(c));
+        printf("Hasil Pythagoras: %1.f", sqrt(c));
+        printf("\n\n");
 
-    //Output Hasil
-    printf("Akar dari %d adalah %1.f\n", c, sqrt(c));
-    printf("Hasil Pythagoras: %1.f", sqrt(c));
+        //Kalo mau lanjut perhitungan
+        printf("Mau lanjut hitung lagi? ");
+        printf("(y/t) >  ");
+        scanf(" %c", &lanjut);
+        printf("\n");
 
+        ulang++;
+    }
+
+    printf("Terima Kasih sudah menggunakan program ini ^_^\n");
+    printf("Semoga terbantu");
+    
     return 0;
 }
